9f8bb1dc5e5d84bf2d90035fa2c1c46a7a8a81ec
dk169 <dk16901@gmail.com>
2020-12-30 20:00:57 +0200

    adding custom error and response

---
+++ Backend/utils/customError.js
@@ -0,0 +1,9 @@
+export default class CustomeError extends Error {
+    constructor(message, code) {
+      super(message)
+      this.code = code
+      this.message = message
+      this.isOperational = true      
+    }
+  }
+  
---
+++ Backend/utils/customResponse.js
@@ -0,0 +1,6 @@
+export default class CustomeResponse {
+  constructor(data) {
+    this.success = 1
+    this.data = data
+  }
+}
